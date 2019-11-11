92ca91c968fed8ebf3d379486c071f212e99f624
Shao-Ching Huang <sch@ucla.edu>
2019-11-10 19:28:44 -0800

    do not use .mode column which chops the column width

--- showall.sh
+++ showall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 if [ -f data.db ]; then
-   sqlite3 data.db -cmd ".headers on" -cmd ".mode column" "select * from urls"
+   #sqlite3 data.db -cmd ".headers on" -cmd ".mode column" "select * from urls"
+   sqlite3 data.db -cmd ".headers on" "select * from urls"
 fi
